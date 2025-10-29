583eb71e43f9a3279eea555503f4e6355dbff9b4
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-10-29 16:42:17 +0800

    完成基于请求头信息的反向代理

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -20,12 +20,15 @@ var (
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
-			s.BindHandler("/", func(r *ghttp.Request) {
+			s.BindHandler("/*", func(r *ghttp.Request) {
 				// 规则一，查看请求头请求的服务
 				service := r.Header.Get("X-Service")
 				if service != "" {
-					proxyHost, _ := svc.GetProxyHost(service)
-					g.Log().Infof(r.Context(), `proxy:"%s" -> backend:"%s"`, r.Request.URL.Host, proxyHost)
+					proxyHost, err := svc.GetProxyHost(service)
+					if err != nil {
+						r.Response.WriteStatus(http.StatusBadRequest, err.Error())
+						return
+					}
 					r.MakeBodyRepeatableRead(false)
 
 					// 创建反向代理
@@ -38,13 +41,14 @@ var (
 								return
 							}
 
-							// 重写请求的 URL 和 Host
+							// 重写请求的 URL、Host 和 请求头
 							req.URL.Scheme = target.Scheme
 							req.URL.Host = target.Host
-							req.Host = target.Host // 设置 Host 头
-							g.Log().Infof(req.Context(), `proxy URL: %s`, req.URL.String())
-							// 可选：添加自定义 Header
+							req.URL.Path = r.URL.Path
+							req.Host = target.Host
 							req.Header.Set("X-Forwarded-For", req.RemoteAddr)
+
+							g.Log().Infof(r.Context(), `[Gateway]: %s -> [%s]: %s://%s%s`, r.GetUrl(), service, req.URL.Scheme, req.URL.Host, req.URL.Path)
 						},
 						ErrorHandler: func(writer http.ResponseWriter, request *http.Request, e error) {
 							g.Log().Errorf(r.Context(), "proxy 失败: %v", e)
@@ -52,8 +56,10 @@ var (
 						},
 					}
 					proxy.ServeHTTP(r.Response.Writer, r.Request)
+				} else {
+					// 规则二，路由匹配
+
 				}
-				// 规则二，路由匹配
 			})
 			s.SetPort(10200)
 			s.Run()
--- internal/service/get_proxy_host.go
+++ internal/service/get_proxy_host.go
@@ -1,5 +1,19 @@
 package service
 
+import (
+	"github.com/gogf/gf/v2/errors/gerror"
+)
+
+var proxyHostMap = map[string]string{
+	"www.baidu.com": "http://www.baidu.com/",
+	"uniauth-gf": "http://localhost:8000/",
+	"uniauth-admin": "http://localhost:5173/",
+}
+
 func GetProxyHost(service string) (string, error) {
-	return "https://www.baidu.com", nil
+	proxyHost, ok := proxyHostMap[service]
+	if !ok {
+		return "", gerror.Newf("未找到 %s 服务。请确认请求头是否传递正确的服务名称！", service)
+	}
+	return proxyHost, nil
 }
