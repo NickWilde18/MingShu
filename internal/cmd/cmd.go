2316fdd6b1e4ffc6d235209121f18ed172783dcf
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-11-05 00:07:42 +0800

    优化本地开发体验

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -4,21 +4,16 @@ import (
 	"context"
 	"time"
 
-	"github.com/gogf/gf/contrib/config/kubecm/v2"
 	_ "github.com/gogf/gf/contrib/nosql/redis/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
-	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gsession"
 
 	"uniauth-gateway/internal/middlewares"
 	"uniauth-gateway/internal/service/auth"
 	"uniauth-gateway/internal/service/proxy"
 )
-
-// 标记是否是本地环境。本地环境不启动 HTTPS 服务器。
-var LOCAL bool = false
 
 var (
 	Main = gcmd.Command{
@@ -27,29 +22,26 @@ var (
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
-
-			// 初始化
-			// 配置配置文件来源，同时确定程序在集群内启动还是外部启动
-			adapter, err := kubecm.New(gctx.GetInitCtx(), kubecm.Config{
-				ConfigMap: "dev-uniauth-gateway-config",
-				DataItem:  "proxy_host_map",
-			})
-			if err != nil {
-				g.Log().Debugf(ctx, "从 Kuebernetes ConfigMap 初始化配置中心失败: %v", err)
-				g.Log().Info(ctx, "从 本地配置文件 初始化配置中心")
-				LOCAL = true
-			} else {
-				g.Cfg("uniauth-gateway-config").SetAdapter(adapter)
-				g.Log().Info(ctx, "从 Kubernetes ConfigMap 初始化配置中心")
-			}
 			// ghttp server 各种特性开启与配置
 			// === 通用配置 ===
+			// 标记是否是本地环境。
+			local := g.Cfg().MustGet(ctx, "server.local", false).Bool()
+			// 日志等级设置
+			if local {
+				if err := g.Log().SetLevelStr("all"); err != nil {
+					g.Log().Errorf(ctx, "设置日志等级 all 错误：%v", err)
+				}
+			} else {
+				if err := g.Log().SetLevelStr("INFO"); err != nil {
+					g.Log().Errorf(ctx, "设置日志等级 INFO 错误：%v", err)
+				}
+			}
 			// 设置 HTTP 服务器
 			s.SetPort(g.Cfg().MustGet(ctx, "server.httpPort").Int())
 			// 设置 Session Age
 			s.SetSessionMaxAge(12 * time.Hour)
 			// === 集群内外部启动配置 ===
-			if !LOCAL {
+			if !local {
 				// 集群外部启动
 				// 启动 HTTPS 服务器
 				s.EnableHTTPS(
