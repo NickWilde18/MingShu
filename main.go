96508233af75507f634c43534061381b6f5b77aa
Yechi Yang <95576172+NickWilde18@users.noreply.github.com>
2025-11-17 23:32:15 +0800

    配置时区

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/gogf/gf/v2/os/gtime"
 
 	"uniauth-gateway/internal/cmd"
 )
 
 func main() {
+	err := gtime.SetTimeZone("Asia/Shanghai")
+    if err != nil {
+        panic(err)
+    }
 	cmd.Main.Run(gctx.GetInitCtx())
 }
